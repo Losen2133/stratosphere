bf1d2d22ba61d48d62e884c640fd7d706988cd3f
Losen2133 <127107157+Losen2133@users.noreply.github.com>
2025-04-30 10:53:43 +0800

    Update flag.service.ts

--- src/app/services/flag.service.ts
+++ src/app/services/flag.service.ts
@@ -7,7 +7,7 @@ export class FlagService {
   private flagUrl = "https://flagsapi.com";
 
   constructor() { }
-
+  
   getFlagUrl(countryCode: string, shiny: boolean, size: number): string {
     if(shiny) {
       return `${this.flagUrl}/${countryCode}/shiny/${size}.png`;
